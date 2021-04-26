bfe6e111e83e67f56c7dd5b0f2e9d1a09d50ea43
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2021-04-26 20:37:47 -0300

    Exemplo aula 18 - Funções personalizadas - Playlist Fundamentos de Python
    Python DS - Criação de funções personalizadas
    #Exemplos de criação de funções simples sem argumentos, funções com argumentos, uso direto das funções no código ou no prompt e uso funções atribuindo valores calculados a variáveis.
    
    Explicações adicionais na playlist a seguir: https://www.youtube.com/playlist?list=PL0XxTDj23A1H-1NxZiOVKkMeCS44EIxih
    
    Mais projetos e tutoriais em:
    http://www.pythonds.com.br
    Instagram: @python.ds
    https://www.youtube.com/channel/UCdpQJDGrM3Xj58ZFF-2UNBA?view_as=subscriber

---
+++ fundamentos/funcoes02.py
@@ -0,0 +1,19 @@
+"""
+Programa cria uma função simples para realizar
+a soma de valores informados nos argumentos,
+em seguida usa a função para somar variáveis
+
+Canal Python DS
+Aula de Fundamentos de Python
+Criação de função com argumentos
+"""
+
+def soma(numero01, numero02):
+    total = numero01 + numero02
+    print(total)
+
+
+a = 2
+b = 3
+
+soma(a, b)
