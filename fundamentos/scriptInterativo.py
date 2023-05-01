9fdb0a9941787c143fc139e8a805588849eda41e
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2023-05-01 10:27:52 -0300

    Create scriptInterativo.py

---
+++ fundamentos/scriptInterativo.py
@@ -0,0 +1,10 @@
+nome = input('Digite seu nome: ')
+print('Olá,', nome, 'bom dia!')
+
+primeiro_numero = int(input('Digite o primeiro número: '))
+segundo_numero = int(input('Digite o segundo número: '))
+
+soma = primeiro_numero + segundo_numero
+
+texto = 'O resultado da soma é:'
+print(texto, soma)
