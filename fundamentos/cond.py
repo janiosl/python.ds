36c83ec34c9103f91099a3c7be241b9336e559f4
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2024-09-15 21:38:07 -0300

    Add files via upload

---
+++ fundamentos/cond.py
@@ -0,0 +1,11 @@
+a = 5
+b = 2
+
+if a == b:
+    print('a é igual a b')
+    soma = a + b
+    print('a + b =', soma)
+else:
+    print('a é diferente de b')
+    diferenca = a - b
+    print('a - b =', diferenca)
