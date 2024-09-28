0b6e3b58496db3dae268dc9e9f28a39cd4ca4263
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2024-09-28 13:11:33 -0300

    Create uso_matriz.py

---
+++ py_poo/uso_matriz.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 20:35:12 2024
+
+@author: janio
+"""
+from matriz import *
+
+#Criação da matriz A vazia
+A = Matriz(3,2)
+A
+A.get_dim()
+A.get_matriz()
+#Preenchimento da matriz de zeros
+A.set_matriz()
+
+#Preenchimento direto da matriz
+A.fill_matriz([1,2,3,4,5,6])
+A.get_matriz()
+print(A)
+
+#Criação da matriz B
+B = Matriz()
+B.get_matriz()
+len(B.get_matriz())
+#Preenchimento de valores na matriz anteriormente preechida com zeros
+B.fill_matriz([1])
+print(B)
+
+
+#Criação da matriz C
+C = Matriz(3,2)
+C.fill_matriz([6,5,4,3,2,1])
+C.get_dim()
+C.get_matriz()
+print(C)
+
+
+#Comparação de dimensões de matrizes
+A == C
+A == B
+C == B
+
+#Operações entre matrizes
+
+#Multiplicação diagonal por escalar
+A.get_matriz()
+len(A.get_matriz())
+A.mult_diag(2)
+
+D = Matriz(4,4)
+D.fill_matriz(list(range(16)))
+print(D)
+
+D.mult_diag(2)
+
+'''
+Resultado
+[0, 1, 2, 3],
+[4, 10, 6, 7],
+[8, 9, 20, 11],
+[12, 13, 14, 30]
+
+Original
+[0, 1, 2, 3],
+[4, 5, 6, 7],
+[8, 9, 10, 11],
+[12, 13, 14, 15]
+'''
+
+#Adição de matrizes
+A.get_matriz()
+C.get_matriz()
+
+A.__add__(C)
+A + B
+A + C
+
+
+N = A + C
+N
+#[[7, 7], [7, 7], [7, 7]]
+
+J = Matriz(2,2)
+J
+Matriz(3,3)
+Matriz(1,1)
+
+#Multiplicação de matrizes
+A * C
+#'Impossível calcular: Dimensões incompatíveis'
+A * B
+#'Impossível calcular: Dimensões incompatíveis'
+
+Z = Matriz(2,3)
+Z
+Z.fill_matriz([3,3,3,3,3,3])
+A.get_matriz()
+A * Z
+
+newA = Matriz(3,2)
+newA.set_matriz()
+newB = Matriz(2,5)
+newB.set_matriz()
+AB = newA * newB
+AB
