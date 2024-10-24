084224fcdceaf927539078ef8c467cdcca4560e0
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2024-10-24 20:45:44 -0300

    Create modulo.py

---
+++ fundamentos/modulo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 17:42:32 2024
+
+@author: janio
+"""
+
+def sqrt(numero):
+    return numero ** (1/2)
+
+
+def sum(a,b):
+    return a+b
+    
+
+def pot(numero):
+    return numero ** 2
