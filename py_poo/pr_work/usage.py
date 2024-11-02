86f34cd7ee22bb31e150507ae2471f03fee309b7
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2024-11-02 14:10:22 -0300

    Create usage.py

---
+++ py_poo/pr_work/usage.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+DeoST AD usage
+@author: janio
+"""
+
+from deost_ad import *
+import pandas as pd
+from matplotlib import pyplot as plt
+
+#Loading data
+entrada = 'gecco.csv'
+gecco = pd.read_csv(entrada)
+
+#Data analysis
+gecco.head()
+gecco.describe()
+
+
+#Subset
+series = gecco.ph
+reference = gecco.event
+plt.plot(series)
+
+#DeoST detector
+#Create DeoST object with series
+ph = GeraTS(serie=series)
+
+#Model
+deost = Evento(s=ph)
+
+#Detection
+deost.detect()
+print(deost)
+
+#Detection visualization
+deost.det_graf()
